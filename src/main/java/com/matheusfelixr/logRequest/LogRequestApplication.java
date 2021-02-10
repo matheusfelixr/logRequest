c80b5630f78a090c3d56c62ab3e400c7f00da135
Matheus Felix Ribeiro <matheusfelixr@hotmail.com>
2021-02-10 01:50:42 -0300

    main

---
+++ src/main/java/com/matheusfelixr/logRequest/LogRequestApplication.java
@@ -0,0 +1,13 @@
+package com.matheusfelixr.logRequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogRequestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/matheusfelixr/logRequest/LogRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusfelixr.logRequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
